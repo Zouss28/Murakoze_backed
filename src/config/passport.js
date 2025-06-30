d91b35eeb99a0093fa9ae61caab833e604d08b4f
Alan <alan@z0u55>
2025-06-30 11:29:46 +0200

    v10.5

--- src/config/passport.js
+++ src/config/passport.js
@@ -5,7 +5,6 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "/api/auth/google/callback",
-    passReqToCallback: true
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
